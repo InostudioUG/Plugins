2af49c1c94c42ebdbf17a0305e80685572bbdcb2
Niels Garve <niels.garve@inostudio.de>
2013-09-10 17:30:07 +0200

    wrapped title-numeric.js within a requirejs module definition

--- sorting/title-numeric.js
+++ sorting/title-numeric.js
@@ -11,17 +11,22 @@
  *  @author <a href="http://sprymedia.co.uk">Allan Jardine</a>
  */
 
-jQuery.extend( jQuery.fn.dataTableExt.oSort, {
-	"title-numeric-pre": function ( a ) {
-		var x = a.match(/title="*(-?[0-9\.]+)/)[1];
-		return parseFloat( x );
-	},
+define([
+    'jquery',
+    'dataTables'
+], function (jQuery) {
+    jQuery.extend(jQuery.fn.dataTableExt.oSort, {
+        'title-numeric-pre': function (a) {
+            var x = a.match(/title="*(-?[0-9\.]+)/)[1];
+            return parseFloat(x);
+        },
 
-	"title-numeric-asc": function ( a, b ) {
-		return ((a < b) ? -1 : ((a > b) ? 1 : 0));
-	},
+        'title-numeric-asc': function (a, b) {
+            return ((a < b) ? -1 : ((a > b) ? 1 : 0));
+        },
 
-	"title-numeric-desc": function ( a, b ) {
-		return ((a < b) ? 1 : ((a > b) ? -1 : 0));
-	}
-} );
+        'title-numeric-desc': function (a, b) {
+            return ((a < b) ? 1 : ((a > b) ? -1 : 0));
+        }
+    });
+});
