de835e63bc8f5f0140b1fe47ea658f1ef923d4a8
Niels Garve <niels.garve@inostudio.de>
2013-10-04 15:40:55 +0200

    wrapped api/fnAddTr within a requireJS module

--- api/fnAddTr.js
+++ api/fnAddTr.js
@@ -6,50 +6,55 @@
  *  @author <a href="http://sprymedia.co.uk">Allan Jardine</a>
  *
  *  @example
- *    
+ *
  */
 
-$.fn.dataTableExt.oApi.fnAddTr = function ( oSettings, nTr, bRedraw ) {
-    if ( typeof bRedraw == 'undefined' )
-    {
-        bRedraw = true;
-    }
-
-    var nTds = nTr.getElementsByTagName('td');
-    if ( nTds.length != oSettings.aoColumns.length )
-    {
-        alert( 'Warning: not adding new TR - columns and TD elements must match' );
-        return;
-    }
-
-    var aData = [];
-    var aInvisible = [];
-    for ( var i=0 ; i<nTds.length ; i++ )
-    {
-        aData.push( nTds[i].innerHTML );
-        if (!oSettings.aoColumns[i].bVisible)
+define([
+    'jquery',
+    'dataTables'
+], function($) {
+    $.fn.dataTableExt.oApi.fnAddTr = function ( oSettings, nTr, bRedraw ) {
+        if ( typeof bRedraw == 'undefined' )
         {
-            aInvisible.push( i );
+            bRedraw = true;
         }
-    }
 
-    /* Add the data and then replace DataTable's generated TR with ours */
-    var iIndex = this.oApi._fnAddData( oSettings, aData );
-    nTr._DT_RowIndex = iIndex;
-    oSettings.aoData[ iIndex ].nTr = nTr;
+        var nTds = nTr.getElementsByTagName('td');
+        if ( nTds.length != oSettings.aoColumns.length )
+        {
+            alert( 'Warning: not adding new TR - columns and TD elements must match' );
+            return;
+        }
 
-    oSettings.aiDisplay = oSettings.aiDisplayMaster.slice();
+        var aData = [];
+        var aInvisible = [];
+        for ( var i=0 ; i<nTds.length ; i++ )
+        {
+            aData.push( nTds[i].innerHTML );
+            if (!oSettings.aoColumns[i].bVisible)
+            {
+                aInvisible.push( i );
+            }
+        }
 
-    // Hidding invisible columns
-    for ( var i = (aInvisible.length - 1) ; i >= 0 ; i-- )
-    {
-		oSettings.aoData[iIndex]._anHidden[ i ] = nTds[aInvisible[i]];
-		nTr.removeChild( nTds[aInvisible[i]] );
-    }
+        /* Add the data and then replace DataTable's generated TR with ours */
+        var iIndex = this.oApi._fnAddData( oSettings, aData );
+        nTr._DT_RowIndex = iIndex;
+        oSettings.aoData[ iIndex ].nTr = nTr;
 
-	// Redraw
-    if ( bRedraw )
-    {
-        this.oApi._fnReDraw( oSettings );
-    }
-};
+        oSettings.aiDisplay = oSettings.aiDisplayMaster.slice();
+
+        // Hidding invisible columns
+        for ( var i = (aInvisible.length - 1) ; i >= 0 ; i-- )
+        {
+            oSettings.aoData[iIndex]._anHidden[ i ] = nTds[aInvisible[i]];
+            nTr.removeChild( nTds[aInvisible[i]] );
+        }
+
+        // Redraw
+        if ( bRedraw )
+        {
+            this.oApi._fnReDraw( oSettings );
+        }
+    };
+});
